5d97b50d1b30df85013e5f61a2cdec17dc93cd9c
Sebastijan Kolenko <sebastijan.kolenko@gmail.com>
2015-06-29 11:18:45 +0200

    php

--- json.php
+++ json.php
@@ -1,4 +1,4 @@
 <?php
-$file = file_get_contents($_GET['http://wwk-3a.cams.wlan-si.net/last_state.json']);
+$file = file_get_contents('http://wwk-3a.cams.wlan-si.net/last_state.json');
 echo $file;
 ?>
